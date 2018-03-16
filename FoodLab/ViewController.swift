c6a873707b9b854f22bceabfbe80bab3ac0edde5
Francisco Hernanedez <cisco@Franciscos-MacBook-Pro.local>
2018-03-15 20:09:49 -0700

    First Commit, setting up

--- FoodLab/ViewController.swift
+++ FoodLab/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import Foundation
+import Alamofire
+import SwiftyJSON
+import SwiftKeychainWrapper
+
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        APITest()
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func APITest(){
+        let retrievedString: String? = KeychainWrapper.standard.string(forKey: "foodApi")
+        
+        let headers: HTTPHeaders = [
+            "X-Mashape-Key": retrievedString!,
+            "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com",
+            "accept": "application/json"
+        ]
+        
+        Alamofire.request("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=5&ranking=", headers: headers).responseJSON { response in
+            debugPrint(response)
+            
+            
+            switch response.result{
+            case .success(let value):
+                let json = JSON(value)
+                print (json)
+            case .failure(let error):
+                print(error)
+            }
+        }
 
-
+    }
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FoodLab' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FoodLab
+	pod 'Firebase/Core'
+	pod 'Firebase/Database'
+	pod 'Alamofire'
+	pod 'SwiftyJSON'
+	pod 'SwiftKeychainWrapper'
+	
+  target 'FoodLabTests' do
+    inherit! :search_paths
+    # Pods for testing
+   
+  end
+
+  target 'FoodLabUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
